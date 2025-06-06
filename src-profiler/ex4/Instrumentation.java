87f38fabc62a95522d0d46d847cce56534952706
Alind Xhyra <alind.xhyra@usi.ch>
2025-06-06 13:16:49 +0000

    Completed ex4

--- src-profiler/ex4/Instrumentation.java
+++ src-profiler/ex4/Instrumentation.java
@@ -1,5 +1,20 @@
 package ex4;
 
+import ch.usi.dag.disl.annotation.Before;
+import ch.usi.dag.disl.marker.BytecodeMarker;
+import ch.usi.dag.disl.marker.BodyMarker;
+
+import ex2.Profiler;
+
 public class Instrumentation {
+    @Before(marker = BytecodeMarker.class, args = "new, newarray, anewarray, multianewarray", scope = "*.*")
+    public static void profileSafeAllocation() {
+        ex4.Profiler.addSafeCount();
+    }
+
+    @Before(marker = BodyMarker.class, scope = "sun.misc.Unsafe.*")
+    public static void profileUnsafeAllocation() {
+        ex4.Profiler.addUnsafeCount();
+    }
 
 }
