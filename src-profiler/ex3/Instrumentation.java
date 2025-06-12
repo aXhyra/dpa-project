c915f38d2318ba85a9a31d37de392d599d39ae21
pako-23 <pasqualepolverino98@gmail.com>
2025-06-12 07:03:40 +0200

    add exercise 3

--- src-profiler/ex3/Instrumentation.java
+++ src-profiler/ex3/Instrumentation.java
@@ -1,5 +1,16 @@
 package ex3;
 
+import ch.usi.dag.disl.annotation.After;
+import ch.usi.dag.disl.marker.BytecodeMarker;
+import ch.usi.dag.disl.dynamiccontext.DynamicContext;
+
 public class Instrumentation {
 
+    @After(marker = BytecodeMarker.class,
+	    args="astore",
+	    scope = "ex3.MainThread.checkAccess")
+    static void variableUpdate(DynamicContext ctx) {
+	if (ctx.getLocalVariableValue(2, String.class).equals("granted"))
+	    System.out.format("Thread: %s\n", Thread.currentThread().getName());
+    }
 }
